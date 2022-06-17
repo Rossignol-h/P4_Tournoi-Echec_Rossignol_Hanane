a7177282ada0051d4c4015564645841b399b9b2c
Rossignol-h <ha.rossignol@laposte.net>
2022-06-17 22:55:32 +0200

    added tournament function management

--- models/tournoi.py
+++ models/tournoi.py
@@ -25,3 +25,37 @@ class Tournoi:
 
     def ajout_tours(self, tour):
         self.tours.append(tour)
+
+# 2-serialize the instanciated players[mail documentation] :
+
+    def serialisation(self):
+        tournoi_serialisé = {
+            "id": self.id,
+            "participants": self.participants,
+            "nom": self.nom,
+            "lieu": self.lieu,
+            "date": self.date,
+            "nb_tours": self.nb_tours,
+            "tours": [],
+            "control_temps": self.control_temps,
+            "remarques": self.remarques
+        }
+        self.nom
+        for tour in self.tours:
+            tournoi_serialisé["tours"].append(tour.serialisation())
+        return tournoi_serialisé
+
+    def ajout_tournoi__db(self):
+        tournoi_serialisé = self.serialisation()
+        table_tournois.insert(tournoi_serialisé)
+
+    def maj_tournoi__db(self):
+        tournoi_db = Query()
+        table_tournois.update(tournoi_db.id == self.id)
+
+    def recup_id_tournoi(self, nom=str):
+        doc = table_tournois.search(Tournoi(nom) == self.nom)
+        return doc[0].doc_id
+
+    def liste_tours(self):
+        return len(self.liste_tours)
